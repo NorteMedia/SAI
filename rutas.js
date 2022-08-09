d9f9c5416dc14312ee6f7619229133a92b6b920a
Francisco López <franck5532@gmail.com>
2022-08-09 07:55:48 -0500

    Count

--- rutas.js
+++ rutas.js
@@ -104,6 +104,17 @@ router.get('/credito/getcreditosactivosejecutivo',(req, res)=>{
     })
 })
 
+router.get('/credito/getcontadorgerencia',(req, res)=>{
+    let sql =`SELECT count(*) FROM sai_prod.cliente where id_gerencia = 5;`
+    
+    conexion.query(sql,(err, rows, fields)=>{
+        if(err) throw err;
+        else{
+            res.json(rows)
+        }
+    })
+})
+
 
 //abono_credito
 router.get('/abonocredito/getall',(req, res)=>{
